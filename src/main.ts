3078e683c4ae5de581cf6eaede89eeb598095035
Bạch Gia Huy <bghuy2003@gmail.com>
2025-01-24 12:26:56 +0700

    set up service

--- src/main.ts
+++ src/main.ts
@@ -2,10 +2,12 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 // const cookieParser = require('cookie-parser');
 import cookieParser from 'cookie-parser';
-import * as dotenv from 'dotenv';
-dotenv.config();
+import { MicroserviceOptions, Transport } from '@nestjs/microservices';
+// import * as dotenv from 'dotenv';
+// dotenv.config();
 
 async function bootstrap() {
+
   const app = await NestFactory.create(AppModule);
 
   const allowedOrigins = [process.env.CLIENT_PRODUCTION_URL, process.env.CLIENT_DEVELOPMENT_URL];
@@ -27,6 +29,20 @@ async function bootstrap() {
 
   app.use(cookieParser());
   await app.listen(process.env.PORT ?? 3000);
+
+  const microservice = await NestFactory.createMicroservice<MicroserviceOptions>(AppModule, {
+    transport: Transport.RMQ,
+    options: {
+      urls: ['amqp://localhost:5672'],
+      queue: 'main_queue',
+      queueOptions: {
+        durable: false,
+      },
+    },
+  });
+  microservice.listen();
+  console.log('Microservice is listening');
+  
 }
 
 bootstrap();
